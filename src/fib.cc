a4b2a2f4aa52147d3dbfff244e43380eb1c8c9c4
Mihai Maruseac <mihaimaruseac@google.com>
2022-07-04 12:44:04 -0700

    Add 2 C++ files
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

---
+++ src/fib.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int fib(int x) {
+  if (x < 2) return 1;
+  return fib(x-1) + fib(x-2);
+}
+
+int main(int argc, char** argv) {
+  std::cout << "fib( 2) = " << fib( 2) << "\n";
+  std::cout << "fib( 3) = " << fib( 3) << "\n";
+  std::cout << "fib( 5) = " << fib( 5) << "\n";
+  std::cout << "fib( 7) = " << fib( 7) << "\n";
+  std::cout << "fib(11) = " << fib(11) << "\n";
+  return 0;
+}
