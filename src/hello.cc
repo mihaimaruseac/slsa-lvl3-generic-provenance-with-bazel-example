0cfc0dbc84c50c6c44df7b089963be29eb79a76b
Mihai Maruseac <mihaimaruseac@google.com>
2022-08-05 10:28:52 -0700

    Add dependency on `{fmt}` and use it
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- src/hello.cc
+++ src/hello.cc
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <fmt/core.h>
 
 int main(int argc, char** argv) {
   std::cout << "Hello from C++!\n";
+  fmt::print("The answer to everything is {}\n", 42);
   return 0;
 }
