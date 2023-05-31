0172781d806ce763c66d04e92686086fdc5716ce
Mihai Maruseac <mihaimaruseac@google.com>
2023-05-31 14:50:24 -0700

    Add a simple java app
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package example;
+
+import java.util.Arrays;
+
+final public class Main {
+
+  public static void main(String args[]) {
+    System.out.println("Hello from Java with args: " + Arrays.toString(args));
+  }
+}
